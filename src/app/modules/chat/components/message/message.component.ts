4cb9e104b8e445b7014e5e829b3a31896a900bd4
Tikhon <alexeev.tix@yandex.ru>
2023-01-08 00:09:04 +0300

    added output username for message

--- src/app/modules/chat/components/message/message.component.ts
+++ src/app/modules/chat/components/message/message.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IMessage } from 'src/app/types';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'chat-message',
@@ -9,15 +10,15 @@ import { IMessage } from 'src/app/types';
 
 export class MessageComponent implements OnInit {
 
-  @Input() data:IMessage
+  @Input() data!:IMessage
 
-  text = ''
-  constructor() { 
-    this.data = { id: "", userId: "", text:"",roomId:'' } 
+  isCurrentUser = false;
+
+  constructor(private authService:AuthService) { 
+    console.log(this.data)
   }
 
   ngOnInit(): void {
-    this.text = this.data.text
-
+    this.isCurrentUser = this.authService.currentUser.value.id === this.data.user.id;
   }
 }
