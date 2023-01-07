bf56ecdca5ecd8134b1d59d797be3c00602b942f
Tikhon <alexeev.tix@yandex.ru>
2023-01-08 00:06:38 +0300

    changed submit message

--- src/app/service/messages.service.ts
+++ src/app/service/messages.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, map } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { SocketService } from './socket.service';
 import { API_URL } from '../constants';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,20 +14,23 @@ export class MessagesService {
 
   constructor(
     private http:HttpClient, 
-    private socketService:SocketService
-  ) { }
+    private socketService:SocketService,
+    private authService:AuthService
+  ) {}
 
   public messages = new BehaviorSubject<IMessage[]>([])
 
-  public getMessages(roomId:string):void {
+  public getMessages(roomId:string) {
     this.http.get<IMessage[]>(`${API_URL}/messages/${roomId}`).subscribe((value => {
       this.messages.next(value)
     }))
   }
 
   public onSubmitMessage(textMessage:string, roomId:string){
-    let socket = this.socketService.socket;
-  
-    socket?.next({text:textMessage, roomId})
+    const socket = this.socketService.socket;
+    const userId = this.authService.currentUser.value.id
+
+    //@ts-ignore
+    socket?.next({text:textMessage, roomId, userId})
   }
 }
