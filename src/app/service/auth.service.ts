c1870ba22a3c8ed6ab97538d98d1878f92889153
Tikhon <alexeev.tix@yandex.ru>
2023-01-08 00:01:44 +0300

    created regist and login methods

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,25 +1,66 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { UserData } from '../types';
-
-export interface IAuthService {
-  login:(user: UserData) => void;
-  logout:(user: UserData) => void;
-  regist:(user: UserData) => void;
-  isAuth:boolean;
-}
+import { AccessUserData, UserData } from '../types';
+import { API_URL } from '../constants';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService implements IAuthService{
 
-  constructor() { }
+export class AuthService {
 
-  isAuth:boolean = false;
+  constructor( 
+    private http:HttpClient,
+    private router:Router
+  ){}
 
-  login(user: UserData){};
+  currentUser = new BehaviorSubject({ id:'', username:'', email:'' })
 
-  logout(user: UserData){};
+  isAuth = new BehaviorSubject(false);
 
-  regist(user: UserData){};
+  private setCurrentUser(data:AccessUserData) {
+    this.isAuth.next(true);
+    this.currentUser.next({ id:data['id'], username:data["username"], email:data["email"] });
+    this.router.navigate([''])
+    localStorage.setItem('token', JSON.stringify(data?.['accessToken']))
+  }
+
+  login(user: UserData){
+    this.http.post<AccessUserData>(`${API_URL}/auth/login`,user, { withCredentials:true }).subscribe((data) => {
+      if(data?.['accessToken']){
+        this.setCurrentUser(data);
+      }else{
+        console.log("Ошибка авторизации")
+      }
+    })
+  };
+
+  logout(){
+    this.http.get<AccessUserData>(`${API_URL}/auth/logout`).subscribe((data) => {
+      if(data){
+        localStorage.removeItem("token")
+        this.router.navigate(['login'])
+      }else{
+        console.log("Ошибка выхода")
+      }
+    })
+  };
+
+  regist(user: UserData){
+    this.http.post<AccessUserData>(`${API_URL}/auth/registration`, user, { withCredentials:true }).subscribe((data) => {
+      if(data?.['accessToken']){
+        this.setCurrentUser(data);
+      }else{
+        console.log("Ошибка регистрации")
+      }
+    })
+  };
+
+    checkAuth(){
+          this.http.get<AccessUserData>(`${API_URL}/auth/refresh`, { withCredentials:true }).subscribe((data) => {
+          this.setCurrentUser(data);
+    })
+  }
 }
